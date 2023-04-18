12d53f8affddb6fb76110a0b23f8d3c807579a3c
Magehawk <anisafridi@outlook.de>
2023-04-19 01:42:10 +0200

    Provide C and C++ Example for illustration

---
+++ src/Main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main(int argc, char* argv[])
+{
+    printf("CMakeTemplate by Magehawk!\nGithub Repository: https://github.com/TheMagehawk/CMakeTemplate.git\n\n");
+
+#ifdef IS_DEBUG
+    printf("Compiled C Example in Debug mode!\n");
+#else
+    printf("Compiled C Example in Release mode!\n");
+#endif
+
+    return 0;
+}
--- src/Main.cpp
+++ src/Main.cpp
@@ -2,7 +2,13 @@
 
 int main(int argc, char* argv[])
 {
-	std::cout << "CMakeTemplate by Magehawk!\n";
+    std::cout << "CMakeTemplate by Magehawk!\n" << "Github Repository: https://github.com/TheMagehawk/CMakeTemplate.git\n\n";
+
+    #ifdef IS_DEBUG
+    std::cout << "Compiled C++ Example in Debug mode!\n";
+    #else
+    std::cout << "Compiled C++ Example in Release mode!\n";
+    #endif
 
 	return 0;
 }
